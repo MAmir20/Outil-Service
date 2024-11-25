4f5b582120417482ed826e58910fa7b5bf7f8807
MAmir20 <mezghani.amir@gmail.com>
2024-11-25 21:26:53 +0100

    Added Discovery Service support

--- src/main/java/com/example/demo/OutilServiceApplication.java
+++ src/main/java/com/example/demo/OutilServiceApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.example.demo.entity.Outil;
 import com.example.demo.service.IOutilService;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OutilServiceApplication implements CommandLineRunner {
 
 	@Autowired
