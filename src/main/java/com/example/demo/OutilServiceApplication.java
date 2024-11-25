acd911fb3104da0f9adf0e592b7ec973fedfff69
MAmir20 <mezghani.amir@gmail.com>
2024-11-25 01:31:30 +0100

    Outil Service done

---
+++ src/main/java/com/example/demo/OutilServiceApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.entity.Outil;
+import com.example.demo.service.IOutilService;
+
+@SpringBootApplication
+public class OutilServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	IOutilService outilService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OutilServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Outil out1 = Outil.builder()
+				.date(new Date())
+				.source("file.pdf")
+				.build();
+		
+		out1 = outilService.addOutil(out1);
+	}
+	
+
+}
